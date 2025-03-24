7979f4a83f8dc764c07eb03b311607782a509d39
Shravya H T <3>
2025-03-24 11:28:43 +0530

    adding files related to methods

---
+++ Director.java
@@ -0,0 +1,11 @@
+
+
+public class Director {
+    
+    public static void main(String [] args){
+        System.out.println("Director()");
+        Producer.actor();
+    }
+    
+}
+//javac Director.java Producer.java Actor.java Audiance.java Script.java Movie.java
